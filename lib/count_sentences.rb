8d66f0f3ff71d9ab3a62406399dd4a88bcc3964e
kevin hernon <kevinhernon@kevins-MacBook-Air.local>
2016-06-10 17:11:27 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?") 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+ def count_sentences
+    #need to split by each punctuation character.
+    #use an is included in alphabet? array
+    #change everything not in nums, alphabet, or space into a new word (BREAK)
+    #split by BREAK
+    self.split(" ").each_with_object(count=0) do |word|
+      if word.end_with?("?", ".", "!")
+        count += 1
+      end      
+    end 
+    count
+end
 
-  end
 end
